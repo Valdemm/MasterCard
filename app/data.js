661e8a0bba4452265880ad218a6607c5429423de
Valdemm <BlackmCompany@yandex.ru>
2023-02-27 14:47:04 +0300

    comit1

---
+++ app/data.js
@@ -0,0 +1,14 @@
+export const cards = [
+  {
+    _id: "first-card",
+    balance: 43123,
+    number: "1432 1245 7609 4356",
+    color: "black",
+  },
+  {
+    _id: "second-card",
+    balance: 43123,
+    number: "1432 1245 7609 4356",
+    color: "#0095FF",
+  },
+];
